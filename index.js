55a3be6d50a766b1b6d2b639015ff0dbbab3c5bf
KoonKinG <KoonKinG@users.noreply.github.com>
2020-11-12 22:46:47 +0700

    A

--- index.js
+++
@@ -1,8 +0,0 @@
-const { respobse, response, request } = require('express');
-const express=require('express');
-const app=express();
-app.listen(1212,() => console.log(' ที่อยู่ คือ 1212'));
-app.use(express.static('MyWeb'));
-app.post('/api',(request,response)=>{
-    console.log(request);
-});
